3ee4044f438a4279722161fd98efb2a8b3c2763c
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2020-10-27 15:24:37 -0300

    update dev's scripts

--- problem-1/index.ts
+++ problem-1/index.ts
@@ -2,4 +2,4 @@ import sumMultiples3and5 from './sumMultiples3and5';
 
 /** Find the sum of all the multiples of 3 or 5 below 1000. */
 
-console.log(sumMultiples3and5(1000));
+console.log(`The solution to problem number 1 is: ${sumMultiples3and5(1000)}`);
--- problem-2/index.ts
+++ problem-2/index.ts
@@ -1,3 +1,3 @@
-import evenFibonacciSum from './evenFibonacciSum'
+import evenFibonacciSum from './evenFibonacciSum';
 
-console.log(evenFibonacciSum(4000000))
+console.log(`The solution to problem number 2 is: ${evenFibonacciSum(4000000)}`);
